4520be7660a6fc3dc917672f72daff2b6fbd5ac4
rona198 <laven255@gmail.com>
2018-02-05 22:28:43 +0800

    Modified firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyBhajGWIRHa-8NSa8krDFpW8VBm13Gv5Vs",
+	    authDomain: "midterm-exam-dfe26.firebaseapp.com",
+	    databaseURL: "https://midterm-exam-dfe26.firebaseio.com",
+	    projectId: "midterm-exam-dfe26",
+	    storageBucket: "midterm-exam-dfe26.appspot.com",
+	    messagingSenderId: "1063825502382"
     }
 };
